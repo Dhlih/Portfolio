a5e7091369cc8c4b17ace1ec8a21b34395a00464
Ifad Yusuf <86540168+Dhlih@users.noreply.github.com>
2024-12-23 23:18:42 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="w-full text-white py-7 px-[3rem]">
-      <div className="max-w-[1100px] flex mx-auto justify-between flex-wrap md:text-left text-center">
+      <div className="max-w-[1100px] flex mx-auto md:justify-between justify-center flex-wrap md:text-left">
         <h3 className="mb-3">Created by Ifad Yusuf</h3>
         <p>Copyrights © 2024 - All Rights Reserved</p>
       </div>
